3a8f0498c04b6933a0bec47659e474e055049cc7
Antonin <flowythe@gmail.com>
2025-06-28 16:33:36 +0200

    Made microsoft login overlay same as the discord one

--- src/modules/app/components/MicrosoftLoginOverlay/MicrosoftLoginOverlay.tsx
+++ src/modules/app/components/MicrosoftLoginOverlay/MicrosoftLoginOverlay.tsx
@@ -2,16 +2,16 @@
 
 import { Button, Flex, Overlay, Title } from "@mantine/core";
 import { IconBrandMinecraft } from "@tabler/icons-react";
-import { Logo } from "~/modules/layout";
 import { signIn } from "next-auth/react";
 
 export function MicrosoftLoginOverlay() {
     // zIndex 100 is navbar, 99 ensures you can interact with navbar still
     return (
         <Overlay center blur={4} zIndex={99}>
-            <Flex direction={"column"} gap={"xl"}>
-                <Logo goHomeOnClick={false} />
-                <Title>Link your Discord to your Minecraft Account!</Title>
+            <Flex direction={"column"} gap={"xl"} maw={"85%"}>
+                <Title order={2} ta={"center"}>
+                    Link your Discord to your Minecraft Account!
+                </Title>
                 <Button leftSection={<IconBrandMinecraft />} size="xl" onClick={() => signIn("microsoft")}>
                     Login via Microsoft
                 </Button>
